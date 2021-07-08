db135814ff0e19f8f9cbdb4be2f1a1d5e9124a50
Progamerz <progamerz@osbot.org>
2021-07-08 20:18:07 +0300

    Started frontend - Finished home, product and product details.

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
+import ProductDetails from "./components/product/ProductDetails";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Container fluid>
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductDetails} exact />
+        </Container>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Product from "./product/Product";
+import Loader from "./layout/Loader";
+import MetaData from "./layout/MetaData";
+
+import { getProducts } from "../actions/productActions";
+
+import { useAlert } from "react-alert";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { loading, products, error, productsCount } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    console.log("USE-EFFECT RERUN");
+    if (error) {
+      return alert.error(error);
+    }
+    dispatch(getProducts());
+  }, [dispatch, alert, error]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={"Buy Best Products Online"} />
+
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Home;
